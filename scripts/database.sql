9ff3bfe410e38820890f5c40e0a9379b7d3c8423
Nicolasbernard2000 <nicobern.play@gmail.com>
2021-12-31 13:12:37 +0100

    DB completion

--- scripts/database.sql
+++ scripts/database.sql
@@ -117,14 +117,14 @@ INSERT INTO language (international_code) VALUES ('fr');
 INSERT INTO promotion (start_date, end_date, percentage) VALUES (STR_TO_DATE('15-12-2021', '%d-%m-%Y'), STR_TO_DATE('10-02-2022', '%d-%m-%Y'), 10);
 INSERT INTO promotion (start_date, end_date, percentage) VALUES (STR_TO_DATE('01-12-2021', '%d-%m-%Y'), STR_TO_DATE('05-02-2022', '%d-%m-%Y'), 25);
 
-INSERT INTO category (label, description, image, promotion) VALUES ('Football', 'Description football', 'soccer.png', 1);
-INSERT INTO category (label, description, image) VALUES ('Basketball', 'Description basketball', 'basketball.png');
-INSERT INTO category (label, description, image) VALUES ('Hockey', 'Description hockey', 'hockey.png');
-INSERT INTO category (label, description, image) VALUES ('Tennis', 'Description tennis', 'tennis.png');
-INSERT INTO category (label, description, image) VALUES ('Badminton', 'Description badminton', 'badminton.png');
-INSERT INTO category (label, description, image) VALUES ('Ski', 'Description ski', 'ski.png');
-INSERT INTO category (label, description, promotion, image) VALUES ('Musculation', 'Description musculation', 2, 'musculation.png');
-INSERT INTO category (label, description, image) VALUES ('Volleyball', 'Description volleyball', 'volleyball.png');
+INSERT INTO category (label, description, image, promotion) VALUES ('Football', 'Football is the most practiced sport worldwide, in addition to being close to a religion in several countries. It is the heart sport of millions of people.', 'soccer.png', 1);
+INSERT INTO category (label, description, image) VALUES ('Basketball', 'Basketball is one of the most popular sports in the world. The best known championship is the NBA in North America with its franchised teams.', 'basketball.png');
+INSERT INTO category (label, description, image) VALUES ('Hockey', 'Hockey is not the most famous sport, but it is played in many countries, such as Belgium or India. Belgium is currently Olympic champion.', 'hockey.png');
+INSERT INTO category (label, description, image) VALUES ('Tennis', 'Tennis is a sport where the greatest players compete in tournaments. Roland-Garros is the most famous one.', 'tennis.png');
+INSERT INTO category (label, description, image) VALUES ('Badminton', 'Badminton is often played non-competitively, and does not seem overwhelming. Yet, played competitively, it is one of the most tiring sports.', 'badminton.png');
+INSERT INTO category (label, description, image) VALUES ('Ski', 'The favorite sport of one of our employees, skiing brings, in addition to sensations, a magnificent view of the rooftops of the world.', 'ski.png');
+INSERT INTO category (label, description, promotion, image) VALUES ('bodybuilding', 'Strength training helps strengthen different muscles. This helps prevent injuries when playing other sports.', 2, 'musculation.png');
+INSERT INTO category (label, description, image) VALUES ('Volleyball', 'Volleyball is one of the most popular sports in the world. Its practice must combine team cohesion as well as a certain endurance.', 'volleyball.png');
 
 /*Football*/
 INSERT INTO product (price, category, description, image) VALUES (10.0, 1, 'Adults football ball. Size 9. FIFA licensed for professional matches.', 'football_ball.jpg');
@@ -144,54 +144,80 @@ INSERT INTO product (price, category, description, image) VALUES (20, 2, 'This 2
 /*Hockey*/
 INSERT INTO product (price, category, description, image) VALUES (40, 3, 'Professional stick, 20% in carbone material. Ideal to deal with the hardest moves in the sport.', 'hockey_stick.jpg');
 INSERT INTO product (price, category, description, image) VALUES (42, 3, 'Adults shin pads. Resists to violent shocks.', 'hockey_shin_pads.jpg');
-INSERT INTO product (price, category, description, image) VALUES (16, 3, 'Adult standard size hockey ball. Official ball used for competitions', 'hockey_ball.jpg');
+INSERT INTO product (price, category, description, image) VALUES (16, 3, 'Adult standard size hockey ball. Official ball used for competitions.', 'hockey_ball.jpg');
 INSERT INTO product (price, category, description, image) VALUES (30, 3, 'Training cones.', 'hockey_cones.jpg');
 
+/*Tennis*/
+INSERT INTO product (price, category, description, image) VALUES (120, 4, 'Professional racket, 255gr of pur technology in your hands to execute the best smashes.', 'tennis_racket.jpg');
+INSERT INTO product (price, category, description, image) VALUES (5.90, 4, 'These tennis balls are ideals for the player looking for a competitive but durable items. Official Wilson US Open balls.', 'tennis_balls.jpg');
+INSERT INTO product (price, category, description, image) VALUES (134.99, 4, 'Do you want to show off your power ? This radar is made for your.', 'tennis_radar.jpg');
+INSERT INTO product (price, category, description, image) VALUES (15, 4, 'Children rackets to discover the world of tennis.', 'tennis_children_racket.jpg');
+INSERT INTO product (price, category, description, image) VALUES (9, 4, 'Children tennis shoes. Perfect for any type of field.', 'tennis_children_shoes.jpg');
+INSERT INTO product (price, category, description, image) VALUES (15, 4, 'You don\'t have a field in you backyards ? This tennis trainer is for you.', 'tennis_trainer.jpg');
+INSERT INTO product (price, category, description, image) VALUES (12, 4, 'Tennis poles to create a field anywhere you want.', 'tennis_pole.jpg');
+INSERT INTO product (price, category, description, image) VALUES (2, 4, 'Perfect bag to put some balls into it.', 'tennis_bag.jpg');
+
+/*Badminton*/
+INSERT INTO product (price, category, description, image) VALUES (22, 5, 'Professional racket, Power, tolerance and resistance are these assets.', 'badminton_racket.jpg');
+INSERT INTO product (price, category, description, image) VALUES (5, 5, 'Thanks to its plastic skirt, this shuttlecock allows stable trajectories, even when you are new to badminton.', 'badminton_shuttlecocks.jpg');
+INSERT INTO product (price, category, description, image) VALUES (60, 5, 'Badminton net to play in a club. Easy to assemble.', 'badminton_net.jpg');
 
 INSERT INTO translation (language, product, label) VALUES (1, 1, 'Ball');
 INSERT INTO translation (language, product, label) VALUES (2, 1, 'Ballon');
-
 INSERT INTO translation (language, product, label) VALUES (1, 2, 'Goalkeeper gloves');
 INSERT INTO translation (language, product, label) VALUES (2, 2, 'Gants de gardien');
-
 INSERT INTO translation (language, product, label) VALUES (1, 3, 'Jersey');
 INSERT INTO translation (language, product, label) VALUES (2, 3, 'Maillot');
-
 INSERT INTO translation (language, product, label) VALUES (1, 4, 'Shoes');
 INSERT INTO translation (language, product, label) VALUES (2, 4, 'Chaussures');
-
 INSERT INTO translation (language, product, label) VALUES (1, 5, 'Shin pads');
 INSERT INTO translation (language, product, label) VALUES (2, 5, 'Protèges tibias');
 
 INSERT INTO translation (language, product, label) VALUES (1, 6, 'Ball');
 INSERT INTO translation (language, product, label) VALUES (2, 6, 'Ballon');
-
 INSERT INTO translation (language, product, label) VALUES (1, 7, 'Basketball hoop');
 INSERT INTO translation (language, product, label) VALUES (2, 7, 'Panier de basket');
-
 INSERT INTO translation (language, product, label) VALUES (1, 8, 'Basketball shirt');
 INSERT INTO translation (language, product, label) VALUES (2, 8, 'Maillot de basket');
-
 INSERT INTO translation (language, product, label) VALUES (1, 9, 'Basketball short');
 INSERT INTO translation (language, product, label) VALUES (2, 9, 'Short de basket');
-
 INSERT INTO translation (language, product, label) VALUES (1, 10, 'Ankle brace');
 INSERT INTO translation (language, product, label) VALUES (2, 10, 'Chevillère');
-
 INSERT INTO translation (language, product, label) VALUES (1, 11, 'Bag');
 INSERT INTO translation (language, product, label) VALUES (2, 11, 'Sac');
 
 INSERT INTO translation (language, product, label) VALUES (1, 12, 'Stick');
 INSERT INTO translation (language, product, label) VALUES (2, 12, 'Bâton');
-
 INSERT INTO translation (language, product, label) VALUES (1, 13, 'Shin pads');
 INSERT INTO translation (language, product, label) VALUES (2, 13, 'Protèges tibias');
-
 INSERT INTO translation (language, product, label) VALUES (1, 14, 'Hockey ball');
 INSERT INTO translation (language, product, label) VALUES (2, 14, 'Balle de hockey');
-
 INSERT INTO translation (language, product, label) VALUES (1, 15, 'Training cones');
 INSERT INTO translation (language, product, label) VALUES (2, 15, 'Cônes d\'entraînement');
+
+INSERT INTO translation (language, product, label) VALUES (1, 16, 'Tennis racket');
+INSERT INTO translation (language, product, label) VALUES (2, 16, 'Raquette de tennis');
+INSERT INTO translation (language, product, label) VALUES (1, 17, 'Tennis balls');
+INSERT INTO translation (language, product, label) VALUES (2, 17, 'Balles de tennis');
+INSERT INTO translation (language, product, label) VALUES (1, 18, 'Radar');
+INSERT INTO translation (language, product, label) VALUES (2, 18, 'Radar');
+INSERT INTO translation (language, product, label) VALUES (1, 19, 'Children rackets');
+INSERT INTO translation (language, product, label) VALUES (2, 19, 'Raquette enfant');
+INSERT INTO translation (language, product, label) VALUES (1, 20, 'Children shoes');
+INSERT INTO translation (language, product, label) VALUES (2, 20, 'Chaussure enfant');
+INSERT INTO translation (language, product, label) VALUES (1, 21, 'Tennis trainer');
+INSERT INTO translation (language, product, label) VALUES (2, 21, 'Entraineur de tennis');
+INSERT INTO translation (language, product, label) VALUES (1, 22, 'Field Poles');
+INSERT INTO translation (language, product, label) VALUES (2, 22, 'Poteaux de terrain');
+INSERT INTO translation (language, product, label) VALUES (1, 23, 'Tennis bag');
+INSERT INTO translation (language, product, label) VALUES (2, 23, 'Sac de tennis');
+
+INSERT INTO translation (language, product, label) VALUES (1, 24, 'Badminton racket');
+INSERT INTO translation (language, product, label) VALUES (2, 24, 'Raquette de badminton');
+INSERT INTO translation (language, product, label) VALUES (1, 25, 'Badminton shuttlecock');
+INSERT INTO translation (language, product, label) VALUES (2, 25, 'Volant de badminton');
+INSERT INTO translation (language, product, label) VALUES (1, 26, 'Badminton net');
+INSERT INTO translation (language, product, label) VALUES (2, 26, 'Fillet de badminton');
 
 INSERT INTO user (email, password, firstname, lastname, birth_date, phone_number, city, postal_code, street, house_number, username, authorities, non_expired, non_locked, credentials_non_expired, enabled)
 VALUES ('thibaut.berg@hotmail.com', '$2a$10$Q40BovIcYkzsz6NtxW8VAe7TqdY9vEzDeWRm/5ht27PIxOAuPcgwG', 'Thibaut', 'Berg', STR_TO_DATE('28/10/2001', '%d/%m/%Y'), '0478782002', 'Marche-en-Famenne', 6900, 'Rue des champs', 53, 'Spike', 'ROLE_USER', 1, 1, 1, 1);
